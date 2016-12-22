5a9371a237319cbbec1aea03aec40a738bc178b6
Eleni Mijalis <elenimijalis@gmail.com>
2016-12-21 21:04:12 -0600

    isolating jpred result

--- parse-jpred.py
+++ parse-jpred.py
@@ -4,12 +4,18 @@ import argparse
 import itertools
 
 def parse_jpred(name, jalview):
-    print 'test'
-    sys.exit()
+    print name
+    for line in jalview:
+        l = line.split('\t')
+        try:
+            if l[1] == 'jnetpred':
+                print l[len(l)-1]
+        except:
+            pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='outputs jpred data in tabular format')
-    parser.add_argument('name', type=argparse.FileType("r"), help='name file')
-    parser.add_argument('jalview', type=argparse.FileType("r"), help='jalview file')
+    parser.add_argument('name', type=str, help='.name file')
+    parser.add_argument('jalview', type=argparse.FileType("r"), help='.jalview file')
     args = parser.parse_args()
     parse_jpred(**vars(args))
